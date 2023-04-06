eb24a15f8628e2201aef5c55aaa1b9d215f6b295
Kevin van Hal <kevinvhal2005@gmail.com>
2023-04-06 18:08:51 +0200

    tweet verwijderen/profiel veranderen met nieuwe css voor forms

--- login.php
+++ login.php
@@ -5,64 +5,7 @@ session_start();
 <html>
 <head>
     <title>Login</title>
-    <style>
-        body {
-            background-color: #f2f2f2;
-        }
-
-        .container {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            height: 100vh;
-        }
-
-        .login-form {
-            background-color: #fff;
-            padding: 40px;
-            border-radius: 10px;
-            box-shadow: 2px 2px 10px rgba(1, 1, 122);, 1 ;
-            max-width: 400px;
-            width: 100%;
-        }
-
-        .login-form label {
-            display: block;
-            margin-bottom: 10px;
-        }
-
-        .login-form input[type="text"],
-        .login-form input[type="password"] {
-            display: block;
-            width: 100%;
-            padding: 10px;
-            border: 1px solid #ddd;
-            border-radius: 5px;
-            margin-bottom: 20px;
-            font-size: 16px;
-        }
-
-        .login-form input[type="submit"] {
-            background-color: #000b54;
-            color: #fff;
-            border: none;
-            border-radius: 5px;
-            padding: 10px;
-            font-size: 16px;
-            cursor: pointer;
-        }
-
-        .error-message {
-            color: #F9B4F6;
-            margin-top: 10px;
-            font-size: 25px;
-        }
-        .success-message{
-            color: #000b54;
-            margin-top: 10px;
-            font-size: 25px
-        }
-    </style>
+    <link rel="stylesheet" href="form.css">
 </head>
 <body>
 <div class="container">
@@ -82,8 +25,8 @@ session_start();
             $query->execute();
 
             if ($query->rowCount() > 0) {
-                $_SESSION['username'] = $username; // add username to session
-                header("Location: post.php"); // redirect to post page
+                $_SESSION['username'] = $username;
+                header("Location: post.php");
                 exit;
             } else {
                 echo '<p class="error-message">Inloggegevens zijn onjuist!</p>';
--- post.php
+++ post.php
@@ -7,8 +7,6 @@ session_start();
 
 <img src="../afbeeldingen/twotter.png" alt="Logo" style="position: absolute; top: 20px; left: 20px; width: 200px; height: 200px;">
 <form method="POST" class="tweetten">
-    titel:<input type="text" name="titelInput">
-    <br>
     tweet:<input type="text" name="tweetInput">
     <br><br>
     <input type="submit" name="submit">
@@ -19,14 +17,13 @@ session_start();
     if (isset($_SESSION['username'])) {
         $username = $_SESSION['username'];
         echo '<a href="logout.php"><span class="oval">logout</span></a><br>';
-        echo '<h3>' . $username . '</h3>';
+        echo '<a href="editprofile.php"><span class="oval">' . $username . '</span></a><br>';
 
     } else {
         echo '<a href="login.php"><span class="oval">login</span></a><br>';
         echo '<a href="register.php"><span class="oval">register</span></a><br>';
     }
     ?>
-
 </div>
 
 <?php
@@ -34,28 +31,54 @@ include_once "databaseconectie.php";
 
 global $dbConnectie;
 
+if(isset($_SESSION['username'])){
+    $voorbereideQuery = $dbConnectie->prepare("
+        SELECT *, (SELECT username FROM profiel WHERE id = tweets.user_id) AS username 
+        FROM tweets;
+    ");
+    $voorbereideQuery->execute([]);
+    $data = $voorbereideQuery->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($data as $item){
+        ?>
+        <div class="DeTweet">
+            Gebruiker: <?php echo $item["username"]?><br>
+            Tekst: <?php echo $item["inhoud"]?><br>
+            <?php
+            if ($_SESSION['username'] == $item["username"]) {
+                echo '<form method="POST"><button type="submit" name="delete" value="' . $item["id"] . '">Verwijder</button></form>';
+            }
+            ?>
+            <br>
+        </div>
+        <?php
+    }
+}
+
 if(isset($_POST["submit"])){
     $query = $dbConnectie->prepare("
-    INSERT INTO tweets (titel, inhoud, user_id)
-    SELECT :placeholderTitel, :phInhoud, id
-    FROM profiel
-    WHERE username = :phUsername
-");
+        INSERT INTO tweets ( inhoud, user_id)
+        SELECT  :phInhoud, id
+        FROM profiel
+        WHERE username = :phUsername
+    ");
     $query->execute([
-        "placeholderTitel" => $_POST["titelInput"],
         "phInhoud" => $_POST["tweetInput"],
         "phUsername" => $_SESSION['username']
     ]);
 
-$voorbereideQuery = $dbConnectie->prepare("SELECT *, (SELECT username FROM profiel WHERE id = tweets.user_id) AS username FROM tweets;");
-$voorbereideQuery->execute([]);
-$data = $voorbereideQuery->fetchAll(PDO::FETCH_ASSOC);
+    header("Refresh:0");
+}
 
-foreach ($data as $item){
-    ?>
-    <div class="DeTweet">
-        Gebruiker: <?php echo $item["username"]?><br>
-        Tekst: <?php echo $item["inhoud"]?><br><br>
+if(isset($_POST["delete"])){
+    $query = $dbConnectie->prepare("
+        DELETE FROM tweets 
+        WHERE id = :phId
+    ");
+    $query->execute([
+        "phId" => $_POST["delete"]
+    ]);
 
-    </div>
-<?php }}
+    header("Refresh:0");
+}
+?>
